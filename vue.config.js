ed2dbce399052ee8c1cd8889fb04a9d08c94e550
andrezma <andrezma.1222@gmail.com>
2024-09-23 13:30:16 -0500

    deshabilitar proxy en vue.config

--- vue.config.js
+++ vue.config.js
@@ -4,17 +4,17 @@ module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
   ],
-  devServer: {
-    proxy: {
-      '/api': {
-        target: process.env.VUE_APP_API_URL, // Usa la variable de entorno o un valor predeterminado
-        changeOrigin: true,
-        pathRewrite: { '^/api': '' },
-        onProxyRes: (proxyRes) => {
-          // Asegúrate de que los encabezados CORS estén en las respuestas
-          proxyRes.headers['Access-Control-Allow-Origin'] = '*';
-        }
-      },
-    },
-  },
+  // devServer: {
+  //   proxy: {
+  //     '/api': {
+  //       target: process.env.VUE_APP_API_URL, // Usa la variable de entorno o un valor predeterminado
+  //       changeOrigin: true,
+  //       pathRewrite: { '^/api': '' },
+  //       onProxyRes: (proxyRes) => {
+  //         // Asegúrate de que los encabezados CORS estén en las respuestas
+  //         proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+  //       }
+  //     },
+  //   },
+  // },
 });
