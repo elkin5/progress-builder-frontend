a77a41c5cadaa78077f565023bc075aadc8948b8
elkin hurtado <syselkin5@gmail.com>
2024-09-23 12:56:14 -0500

    Poner la variable de entorno de la api en el target

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,3 @@
-// const { defineConfig } = require('@vue/cli-service')
-// module.exports = defineConfig({
-//   transpileDependencies: [
-//     'vuetify'
-//   ]
-// })
-
 const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
@@ -14,7 +7,7 @@ module.exports = defineConfig({
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: process.env.VUE_APP_API_URL || 'http://localhost:3000', // Usa la variable de entorno o un valor predeterminado
         changeOrigin: true,
         pathRewrite: { '^/api': '' },
         onProxyRes: (proxyRes) => {
@@ -25,4 +18,3 @@ module.exports = defineConfig({
     },
   },
 });
-
